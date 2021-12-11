a6b6aa18766311a7a219d092f3e98cda6b9fd2cc
qukai518 <www.163.com>
2021-12-11 11:05:32 +0800

    更新 jx_lhb.js

--- js/jx_lhb.js
+++ js/jx_lhb.js
@@ -44,19 +44,19 @@ const BASE_URL = 'https://m.jingxi.com/cubeactive/steprewardv3'
       '活动入口：京喜app-》我的-》京喜领88元红包\n' +
       '助力逻辑：先自己京东账号相互助力，如有剩余助力机会，则助力作者\n' +
       '温馨提示：如提示助力火爆，可尝试寻找京东客服')
-  let res = await getAuthorShareCode('')
-  if (!res) {
-    $.http.get({url: ''}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
-    await $.wait(1000)
-    res = await getAuthorShareCode('')
-  }
-  if (res && res.activeId) $.activeId = res.activeId;
-  let res2 = await getAuthorShareCode('')
-  if (!res2) {
-    await $.wait(1000)
-    res2 = await getAuthorShareCode('')
-  }
-  $.authorMyShareIds = [...((res && res.codes) || []),...(res2 || [])];
+  // let res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/jxhb.json')
+  // if (!res) {
+    // $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jxhb.json'}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
+    // await $.wait(1000)
+    // res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jxhb.json')
+  // }
+  // if (res && res.activeId) $.activeId = res.activeId;
+  // let res2 = await getAuthorShareCode('https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/jxhb.json')
+  // if (!res2) {
+    // await $.wait(1000)
+    // res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/jxhb.json')
+  // }
+  // $.authorMyShareIds = [...((res && res.codes) || []),...(res2 || [])];
   //开启红包,获取互助码
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
