1a77f229dfae86646d9a42d29ce533042dab9523
qukai518 <www.163.com>
2021-12-14 19:24:00 +0800

    更新 jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -7,8 +7,16 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  '',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '',//账号二的好友shareCode，不同好友中间用@符号隔开
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',//账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',//账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
+	',@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==@FxGBgPRcYSaoYZHYlEtQxA==',
 ]
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
